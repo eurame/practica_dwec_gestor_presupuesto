4e0221459ab9fb4efcd6e64d6ea6b53afe99df81
eurame <euridice.amat@gmail.com>
2021-11-17 16:16:12 +0100

    pru

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,3 +1,5 @@
+import * as gestionPresupuesto from './gestionPresupuesto.js'
+
 'use strict';
 
 function mostrarDatoEnId(idElemento, valor) {
@@ -85,10 +87,65 @@ function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo){
     
 }
 
+/*
+Mostrar el presupuesto en div#presupuesto (funciones mostrarPresupuesto y mostrarDatoEnId)
+Mostrar los gastos totales en div#gastos-totales (funciones calcularTotalGastos y mostrarDatoEnId)
+Mostrar el balance total en div#balance-total (funciones calcularBalance y mostrarDatoEnId)
+Borrar el contenido de div#listado-gastos-completo, para que el paso siguiente no duplique la información. Puedes utilizar innerHTML para borrar el contenido de dicha capa.
+Mostrar el listado completo de gastos en div#listado-gastos-completo (funciones listarGastos y mostrarGastoWeb)
+*/
 
+
+function repintar(){
+
+    let actualPres = gestionPresupuesto.mostrarPresupuesto();
+    gestionPresupuestoWeb.mostrarDatoEnId('presupuesto',actualPres);
+
+    let gTupd = gestionPresupuesto.calcularTotalGastos();
+    gestionPresupuestoWeb.mostrarDatoEnId("gastos-totales",gTupd);
+
+    let bTupd = gestionPresupuesto.calcularBalance();
+    gestionPresupuestoWeb.mostrarDatoEnId("balance-total",bTupd);
+
+    let listGasBorra = document.getElementById('listado-gastos-completo');
+    listGasBorra.innerHTML='';
+
+    let listadoG = gestionPresupuesto.listarGastos();
+    for (let a of listadoG)
+    gestionPresupuestoWeb.mostrarGastoWeb("listado-gastos-completo", a);
+}
+
+function actualizarPresupuestoWeb(){
+
+}
+
+function nuevoGastoWeb(){
+
+}
+
+function EditarHandle(){
+
+}
+
+function BorrarHandle(){
+
+
+}
+
+function BorrarEtiquetasHandle(){
+
+    
+}
 
 export {
     mostrarDatoEnId,
     mostrarGastoWeb,
-    mostrarGastosAgrupadosWeb
+    mostrarGastosAgrupadosWeb,
+    repintar,
+    actualizarPresupuestoWeb,
+    nuevoGastoWeb,
+    EditarHandle,
+    BorrarHandle,
+    BorrarEtiquetasHandle
+
 }
